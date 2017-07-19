bf848248ec29b4b13fad2e59483ebe25cb072a0d
drklrd <srvbhattarai@gmail.com>
2017-07-19 20:42:15 +0545

    add css stylings

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title>@yield('title')</title>
-		<link rel="stylesheet" type="text/css" href="{{  URL::to('css/app.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{  URL::to('css/main.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{  URL::to('css/common.css') }}">
+		<link rel="stylesheet" type="text/css" href="{{  URL::to('css/form.css') }}">
 		@yield('styles')
 	</head>
 
